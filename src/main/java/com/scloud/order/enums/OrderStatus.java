3e71eb06ae86f60dba921f65ef5c1b5fb19d152f
Jason <348842258@qq.com>
2020-03-11 00:39:55 +0800

    feat: :sparkles: 初始化枚举类

---
+++ src/main/java/com/scloud/order/enums/OrderStatus.java
@@ -0,0 +1,18 @@
+package com.scloud.order.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum OrderStatus {
+    NEW(0, "新建"),
+    FINISH(1, "完结"),
+    CANCEL(2, "取消"),
+    ;
+    private Integer code;
+    private String message;
+
+    OrderStatus(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
