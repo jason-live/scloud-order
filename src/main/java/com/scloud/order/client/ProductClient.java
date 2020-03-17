08061423f1fd3b4748921f067831adae908e1e95
Jason <348842258@qq.com>
2020-03-17 23:39:40 +0800

    feat: :sparkles: 1.5.8 口库存

--- src/main/java/com/scloud/order/client/ProductClient.java
+++ src/main/java/com/scloud/order/client/ProductClient.java
@@ -1,5 +1,6 @@
 package com.scloud.order.client;
 
+import com.scloud.order.dto.CartDto;
 import com.scloud.order.entity.ProductInfo;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,4 +17,7 @@ public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
     public List<ProductInfo> listForOrder(@RequestBody  List<String> productIdList);
+
+    @PostMapping("/product/decreaseStock")
+    public void decreaseStock(@RequestBody List<CartDto> cartDtoList);
 }
