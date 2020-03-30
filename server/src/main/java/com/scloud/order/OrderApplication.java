789680faa30370688928ed9b859b681213799e3c
Jason <348842258@qq.com>
2020-03-31 00:07:19 +0800

    feat: :sparkles: 修改版本回到M3

--- server/src/main/java/com/scloud/order/OrderApplication.java
+++ server/src/main/java/com/scloud/order/OrderApplication.java
@@ -3,8 +3,7 @@ package com.scloud.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
