5d402a7739f25c0a866e88912defc5e391e448d7
Jason <348842258@qq.com>
2020-04-09 01:37:11 +0800

    feat: :sparkles: 新增熔断测试

---
+++ server/src/main/java/com/scloud/order/controller/HystrixController.java
@@ -0,0 +1,32 @@
+package com.scloud.order.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@RestController
+@DefaultProperties(defaultFallback = "defaultFallback")
+public class HystrixController {
+
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
+    @GetMapping("/getProductInfoList")
+    public String getProductInfoList() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForObject("http://127.0.0.1:8081/product/listForOrder", Arrays.asList("157875196366160022"), String.class);
+    }
+
+    public String fallback() {
+        return "太拥挤了，请稍后再试";
+    }
+
+    public String defaultFallback() {
+        return "默认注解：太拥挤了，请稍后再试";
+    }
+}
