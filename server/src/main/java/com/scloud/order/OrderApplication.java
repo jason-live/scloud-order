9ac0d1722a4c80330adca401e66012433b1b84c3
Jason <348842258@qq.com>
2020-04-09 22:25:43 +0800

    feat: :sparkles: 熔断完结

--- server/src/main/java/com/scloud/order/OrderApplication.java
+++ server/src/main/java/com/scloud/order/OrderApplication.java
@@ -6,11 +6,15 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.scloud.product.client")
 @EnableCircuitBreaker
+@ComponentScan(basePackages = "com.scloud")
+@EnableHystrixDashboard
 //@SpringCloudApplication
 public class OrderApplication {
 
